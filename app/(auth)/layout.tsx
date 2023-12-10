65a78eb0a2aacba97dd89204a0ac95be18867887
Mohammad Yusuf KHAN <mohammadyusuf.khan@amadeus.com>
2023-12-10 13:00:53 +0530

    add auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import React from 'react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
