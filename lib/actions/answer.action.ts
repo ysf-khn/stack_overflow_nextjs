0c842227a740e2b4a121c06cbd23d9d95c1514aa
Yusuf <yusufmohd72@gmail.com>
2024-02-23 21:36:05 +0530

    All answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -5,18 +5,32 @@ import { connectDB } from "../mongoose";
 import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
 import Answer from "@/database/answer.model";
 import Question from "@/database/question.model";
-// import User from "@/database/user.model";
 
+export async function getAllAnswers(params: GetAnswersParams) {
+  try {
+    connectDB();
 
+    const { questionId } = params;
+
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
+
+    return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
     connectDB();
 
     const { content, author, question, path } = params;
-
-    const newAnswer = new Answer({ content, author, question, path });
-    // const answer = await Answer.create({ content, author, question, path });
+    //
+    // const newAnswer = Answer({ content, author, question, path });
+    const newAnswer = await Answer.create({ content, author, question, path });
 
     // Add the answer to the question's answer qrray
     await Question.findByIdAndUpdate(question, {
