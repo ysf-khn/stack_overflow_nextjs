5430158f27e48c0051ab4e767d166b2c8c7a0c35
Yusuf <yusufmohd72@gmail.com>
2024-02-29 21:39:23 +0530

    top questions & tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,59 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
 
-const hotQuestions = [
-  {
-    _id: '1',
-    title: "How do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: '2',
-    title: "How do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: '3',
-    title: "How do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: '4',
-    title: "How do I use express as a custom server in NextJS?",
-  },
-  {
-    _id: '5',
-    title: "How do I use express as a custom server in NextJS?",
-  },
-];
 
-const popularTags = [
-  {
-    _id: '1',
-    name: "javascript",
-    totalQuestions: 5,
-  },
-  {
-    _id: '2',
-    name: "React",
-    totalQuestions: 5,
-  },
-  {
-    _id: '3',
-    name: "next",
-    totalQuestions: 5,
-  },
-  {
-    _id: '4',
-    name: "vue",
-    totalQuestions: 2,
-  },
-  {
-    _id: '5',
-    name: "redux",
-    totalQuestions: 10,
-  },
-];
+const RightSidebar = async() => {
+const hotQuestions = await getHotQuestions();
+const popularTags = await getTopPopularTags();
 
-const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border shadow-light-300 custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 dark:shadow-none max-xl:hidden">
       <div className="">
@@ -63,7 +18,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">
@@ -88,7 +43,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
