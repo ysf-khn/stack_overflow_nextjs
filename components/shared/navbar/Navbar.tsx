e45bbb8dcfc408dc662d71f7fec50ede789af82a
Yusuf <yusufmohd72@gmail.com>
2024-03-04 16:29:18 +0530

    global search

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import GlobalSearch from "./search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -21,7 +21,9 @@ const Navbar = () => {
           Dev <span className="text-primary-500">OverFlow</span>
         </p>
       </Link>
+
       <GlobalSearch/>
+      
       <div className="flex-between gap-5">
         <Theme/>
         <SignedIn>
